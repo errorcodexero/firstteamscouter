46d34efe945e4ec98168b458fba7905a4ade2760
tom.sommerville <tom.sommerville@gmail.com>
2015-03-03 21:41:15 -0800

    Adding database backup scripts. Updated gitignore to exclude the backup directories.

---
+++ db-backup.bat
@@ -0,0 +1,1 @@
+adb backup -f data.ab -noapk com.wilsonvillerobotics.firstteamscouter
---
+++ db-unzip.sh
@@ -0,0 +1,1 @@
+dd if=data.ab bs=1 skip=24 | python -c "import zlib,sys;sys.stdout.write(zlib.decompress(sys.stdin.read()))" | tar -xvf -
